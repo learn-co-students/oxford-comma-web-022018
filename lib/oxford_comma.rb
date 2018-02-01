91931df2184570cf12b4713d748eafc8c8255c2e
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-01 18:49:06 -0500

    Oxford Comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+    str = ""
+    i = 0
+    if array.length == 1
+        str = array[0]
+    elsif array.length == 2
+        str += array[0] + " and " + array[-1]
+    else
+        while i < array.length
+            if array[i] == array[-1]
+                str += ", and "
+            elsif i >= 1 && i < array.length
+                str +=  ", "
+            end
+            str += array[i]
+            i+= 1
+        end
+    end
+    str
 end
