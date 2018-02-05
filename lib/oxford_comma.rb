dbbf43c7ff03aa7406a047377d36b49263fc4bdc
alec-petros <a.petros@verizon.net>
2018-02-05 17:52:07 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  ret_string = ""
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    ret_string = array.join(" and ")
+  else
+    count = array.size
+    array.each {|name|
+      if count > 2
+        ret_string << "#{name}, "
+        count -= 1
+      elsif count == 2
+        ret_string << "#{name}, and "
+        count -= 1
+      elsif count == 1
+        ret_string << "#{name}"
+      end
+    }
+  end
+  ret_string
 end
